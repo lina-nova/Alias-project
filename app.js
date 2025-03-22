81d96c97cf2ba200a14c11e363b7aae6b8e474da
Oleksii Myronenko <mironenkoalexey@gmail.com>
2025-03-22 12:25:09 +0200

    Add welcome page

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const myElement = 4;
+let anotherElement = 5;
+
+const myFunction = () => {
+    console.log('Hello worlds');
+}
+
+const showWelcomePage = () => {
+    const root = document.getElementById('root');
+    root.innerHTML = `
+        <div class="page1-container">
+            <h1>АЛІАС</h1>
+            <h2>ГРА В СЛОВА</h2>
+            <a href="/newgame" class="page1-button">НОВА ГРА</a>
+        </div>
+    `
+}
+
+showWelcomePage();
